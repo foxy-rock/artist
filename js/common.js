4f2412b86bb1f31e0bfd9fcf5b6e672746e58945
foxy-rock <117028181+foxy-rock@users.noreply.github.com>
2025-05-20 21:14:32 +0500

    Add files via upload

---
+++ js/common.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-jury', {
+  // Optional parameters
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 3.5,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+});
